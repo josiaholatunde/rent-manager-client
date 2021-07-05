c6243f8be5e2819ec1850fab4f6ffc3fdcc3b6fe
Ogunboyejo Olatunde <oogunnboyejo@teamapt.com>
2021-07-05 06:36:38 +0100

    fix: Slight change

--- routes/api/authRoutes.js
+++ routes/api/authRoutes.js
@@ -4,13 +4,6 @@ const { registrationValidator, loginValidator } = require('../../validators/auth
 
 const authRoutes = (app) => {
 
-    app.get('/', (req, res, next) => {
-        return res.json({
-            version: '1.0',
-            description: 'Kwaba Loans Root Route'
-        })
-    })
-
     app.post('/api/v1/register', registrationValidator(), registerUser);
     app.post('/api/v1/login', loginValidator(), loginUser);
 }
